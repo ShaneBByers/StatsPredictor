676004aec2c1088a6bc0a11b37f3c7f519592670
shanebbyers <contact@shane-byers.com>
2020-03-01 13:24:33 -0700

    Further work on SMTP handler.

--- Logger.py
+++ Logger.py
@@ -1,6 +1,9 @@
 import logging
 import Constants
-from logging.handlers import RotatingFileHandler, SMTPHandler, MemoryHandler
+import smtplib
+from datetime import datetime
+from email.message import EmailMessage
+from logging.handlers import RotatingFileHandler, BufferingHandler
 
 
 class Logger:
@@ -29,16 +32,16 @@ class Logger:
         self.log.addHandler(file_handler)
 
     def add_email_handler(self):
-        email_handler = SMTPHandler(Constants.LOGGING_EMAIL_HOST,
-                                    Constants.LOGGING_FROM_EMAIL,
-                                    [Constants.LOGGING_TO_EMAIL],
-                                    "PREDICTOR WARNING/ERROR",
-                                    credentials=(Constants.LOGGING_EMAIL_USERNAME,
-                                                 Constants.LOGGING_EMAIL_PASSWORD))
+        email_handler = BufferingSMTPHandler(Constants.LOGGING_EMAIL_HOST,
+                                             Constants.LOGGING_EMAIL_USERNAME,
+                                             Constants.LOGGING_EMAIL_PASSWORD,
+                                             Constants.LOGGING_FROM_EMAIL,
+                                             Constants.LOGGING_TO_EMAIL,
+                                             "PREDICTOR WARNINGS/ERRORS",
+                                             100)
+        email_handler.setLevel(logging.WARNING)
         email_handler.setFormatter(self.simple_format)
-        memory_handler = MemoryHandler(100, target=email_handler)
-        memory_handler.setLevel(logging.WARNING)
-        self.log.addHandler(memory_handler)
+        self.log.addHandler(email_handler)
 
     def setup_local(self):
         self.add_console_handler()
@@ -52,3 +55,35 @@ class Logger:
     def setup_testing(self):
         self.add_console_handler()
         self.add_email_handler()
+
+
+class BufferingSMTPHandler(BufferingHandler):
+
+    def __init__(self, host, username, password, from_email, to_email, subject, capacity):
+        BufferingHandler.__init__(self, capacity)
+        self.host = host
+        self.username = username
+        self.password = password
+        self.from_email = from_email
+        self.to_email = to_email
+        self.subject = subject
+        self.buffer = []
+
+    def flush(self):
+        if len(self.buffer) > 0:
+            msg = EmailMessage()
+            msg['From'] = self.from_email
+            msg['To'] = ','.join(self.to_email)
+            msg['Subject'] = self.subject
+            msg['Date'] = datetime.now()
+            content = ""
+            for record in self.buffer:
+                content += self.format(record) + "\n"
+            msg.set_content(content)
+            print(content)
+            server = smtplib.SMTP(self.host)
+            server.starttls()
+            server.login(self.username, self.password)
+            server.send_message(msg)
+            server.quit()
+            self.buffer = []
